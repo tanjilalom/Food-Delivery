748c12aaaa5c1127c7b3c1a41027dfaba7c4406a
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-06-27 16:36:00 +0600

    model added

--- lib/model/category_model.dart
+++ lib/model/category_model.dart
@@ -11,10 +11,12 @@ class catagory_item {
 }
 
 List<catagory_item> category = [
-  catagory_item(image: 'assets/romaine-lettuce.jpg', name: 'Lettucce', stock: 12),
-  catagory_item(image: 'assets/broccoli.jpg', name: 'Broccoli', stock: 15),
-  catagory_item(image: 'assets/romaine-lettuce.jpg', name: 'banana', stock: 42),
-  catagory_item(image: 'assets/broccoli.jpg', name: 'orange', stock: 36),
-  catagory_item(image: 'assets/romaine-lettuce.jpg', name: "Apple", stock: 70),
-  catagory_item(image: 'assets/broccoli.jpg', name: 'Lettucce', stock: 46),
+  catagory_item(image: 'assets/category/vegetables.png', name: 'Vegetable', stock: 12),
+  catagory_item(image: 'assets/category/fruits.jpg', name: 'Fruits', stock: 15),
+  catagory_item(image: 'assets/category/Sweets.jpg', name: 'Sweets', stock: 12),
+  catagory_item(image: 'assets/category/Dairy_product.jpg', name: 'Dairy product', stock: 36),
+  catagory_item(image: 'assets/category/meet.jpg', name: "Meat and poultry", stock: 70),
+  catagory_item(image: 'assets/category/Seafood.png', name: 'Seafood', stock: 46),
+  catagory_item(image: 'assets/category/Snacks.jpg', name: 'Snacks', stock: 23),
+  catagory_item(image: 'assets/category/oil.jpg', name: 'Oils and Fats', stock: 53),
 ];
--- lib/model/vegname_model.dart
+++ lib/model/vegname_model.dart
@@ -1,5 +1,20 @@
-class vegname{
+class vegname {
+  String image;
+  String name;
+  String price;
 
-
-
+  vegname({
+    required this.image,
+    required this.name,
+    required this.price,
+  });
 }
+
+List<vegname> vegetable = [
+  vegname(name: 'Sprouting broccoli', price: '1.10€/kg', image: 'assets/product/Vegetables/broccoli.jpg'),
+  vegname(name: 'Romaine Lettuce', price: '1.10€/piece', image: 'assets/product/Vegetables/romaine-lettuce.jpg'),
+  vegname(name: 'Beetroot', price: '1.50€/kg', image: 'assets/product/Vegetables/Beetroot.jpg'),
+  vegname(name: 'Capsicums', price: '1.00€/kg', image: 'assets/product/Vegetables/capsicums.jpg'),
+  vegname(name: 'Cauliflower', price: '1.30€/kg', image: 'assets/product/Vegetables/cauliflower.jpg'),
+  vegname(name: 'Pumpkins', price: '0.10€/kg', image: 'assets/product/Vegetables/pumpkin.jpg'),
+];
